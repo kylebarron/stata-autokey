0187e344c37846d6994dcf055f40851d66fd67e9
Kyle Barron <barronk@mit.edu>
2017-07-17 11:36:16 -0400

    Adjust chunk option to work with //, ///, and /* ... */ in Stata console

--- code/run_stata_chunk.py
+++ code/run_stata_chunk.py
@@ -2,7 +2,6 @@
 import time
 import re
 shortdelay = 0.2
-shorterdelay = 0.1
 
 current = window.get_active_title()
 
@@ -12,121 +11,28 @@ if re.search(r"Atom", current) and re.search(r"^[\w\-\. ]+\.do", current):
     cmd = clipboard.get_selection()
 
     if re.search(r"(//|///|/\*.*\*/)", cmd):
+        # There's commands in that chunk of code that are unreadable in Stata console
+        # Try to adjust those commands to not have comments:
         
-        time.sleep(shortdelay)
-        # Open up a new do-file editor
-        keyboard.send_keys("<ctrl>+9")
-        time.sleep(shortdelay)
-        # Paste text
-        keyboard.send_keys(cmd)
-        time.sleep(shortdelay)
-        # Run the file
-        keyboard.send_keys("<ctrl>+d")
-        # Undo the paste
-        time.sleep(shortdelay)
-        keyboard.send_keys("<ctrl>+z")
-        # Close the do-file editor
-        time.sleep(shortdelay)
-        keyboard.send_keys("<ctrl>+w")
-        # # Don't save changes
-        # keyboard.send_keys("<left>")
-        # time.sleep(shorterdelay)
-        # keyboard.send_keys("<left>")
-        # time.sleep(shorterdelay)
-        # keyboard.send_keys("<enter>")
-        # time.sleep(shorterdelay)
-        time.sleep(shortdelay)
-        window.close("Do-file Editor:")
+        # If ///, delete spaces and the \n
+        # If //, delete spaces and text up to but not including the \n
+        # If /*, delete everthing until */
 
-        time.sleep(shortdelay)
-        window.activate(current)
-        # keyboard.send_keys("<escape>")
+        cmd = re.sub(r"\s*(///)\s*\n\s*", " ", cmd)
+        cmd = re.sub(r"\s*//.*\n", " \n", cmd)
+        cmd = re.sub(r"/\*.*\*/", " ", cmd)
+        cmd = re.sub(r"[\t ]+", " ", cmd)
+        
+    time.sleep(shortdelay)
+    keyboard.send_keys(cmd)
+    keyboard.send_keys("<enter>")
 
-    else:
-        time.sleep(shortdelay)
-        keyboard.send_keys(cmd)
-        keyboard.send_keys("<enter>")
-
-        time.sleep(shortdelay)
-        window.activate(current)
-        keyboard.send_keys("<escape>")
+    time.sleep(shortdelay)
+    window.activate(current)
+    keyboard.send_keys("<escape>")
 
 else:
     keyboard.send_keys("<ctrl>+r")
 
 
 
-# cmd = clipboard.get_selection()
-#
-# time.sleep(shortdelay)
-# keyboard.send_keys(cmd)
-# keyboard.send_keys("<enter>")
-#
-# time.sleep(shortdelay)
-# window.activate(current)
-# keyboard.send_keys("<escape>")
-#
-
-
-#Stiuplations
-# only go through dofile editor when necessary (///)
-# make new dofile editor with ctrl-9
-
-
-
-# If ///, the delete spaces and the \n
-# If //, delete spaces and text up to but not including the \n
-# If /*, delete everthing until */
-
-
-
-
-
-
-
-# if re.search(r"Atom", current) and re.search(r"^[\w\-\. ]+\.do", current):
-#     window.activate("Stata")
-#
-#     cmd = clipboard.get_selection()
-#
-#     if re.search(r"(//|///|/\*.*\*/)", cmd):
-#         time.sleep(shortdelay)
-#         # Open up a new do-file editor
-#         keyboard.send_keys("<ctrl>+9")
-#         time.sleep(shortdelay)
-#         # Paste text
-#         keyboard.send_keys(cmd)
-#         time.sleep(shortdelay)
-#         # Run the file
-#         keyboard.send_keys("<ctrl>+d")
-#         # Undo the paste
-#         time.sleep(shortdelay)
-#         keyboard.send_keys("<ctrl>+z")
-#         # Close the do-file editor
-#         time.sleep(shortdelay)
-#         keyboard.send_keys("<ctrl>+w")
-#         # # Don't save changes
-#         # keyboard.send_keys("<left>")
-#         # time.sleep(shorterdelay)
-#         # keyboard.send_keys("<left>")
-#         # time.sleep(shorterdelay)
-#         # keyboard.send_keys("<enter>")
-#         # time.sleep(shorterdelay)
-#         time.sleep(shortdelay)
-#         window.close("Do-file Editor:")
-#
-#         time.sleep(shortdelay)
-#         window.activate(current)
-#         # keyboard.send_keys("<escape>")
-#
-#     else:
-#         time.sleep(shortdelay)
-#         keyboard.send_keys(cmd)
-#         keyboard.send_keys("<enter>")
-#
-#         time.sleep(shortdelay)
-#         window.activate(current)
-#         keyboard.send_keys("<escape>")
-#
-# else:
-#     keyboard.send_keys("<ctrl>+r")
